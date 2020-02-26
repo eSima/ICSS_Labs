fa9475c4f7ffe4eec418807205aacc022b7c3bb3
eSima <alenasim98@mail.ru>
2020-02-26 09:52:31 +0300

    fix4lab5ex

--- WinForms/lab4/Lab4_WindowsFormsEx5/WindowsFormsLab4Ex5/Form1.cs
+++ WinForms/lab4/Lab4_WindowsFormsEx5/WindowsFormsLab4Ex5/Form1.cs
@@ -17,14 +17,30 @@ namespace WindowsFormsLab4Ex5
         public string SolData
         {
             get { return richTextBox1.Text; }
-          
-            set {
+
+            set
+            {
                 string[] counts = value.Split(':');
-                Results res = new Results();
-                res.first = Math.Round(Math.Sin(double.Parse(counts[0])), 2);
-                res.second = Math.Round(Math.Sin(double.Parse(counts[1])), 2);
-                results.Add(res);
                 label2.Text = "Левая граница:" + counts[0] + " правая граница:" + counts[1];
+                int first = (int)Math.Ceiling(double.Parse(counts[0]) * 100);
+                int last = (int)Math.Ceiling(double.Parse(counts[1]) * 100);
+                for (int i = first; i < last; i += 10)
+                {
+                    Results res = new Results();
+                    res.first = i / 100.0;
+                    res.second = Math.Round(Math.Sin(res.first), 2);
+                    results.Add(res);
+                }
+
+
+                if (last / 100 != last)
+                {
+                    Results res = new Results();
+                    res.first = double.Parse(counts[1]);
+                    res.second = Math.Round(Math.Sin(res.first), 2);
+                    results.Add(res);
+                }
+
 
                 StringBuilder sb = new StringBuilder();
                 foreach (Results item in results)
